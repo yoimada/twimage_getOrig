c4c4f29874b3a7f81850780ace0510da5be14185
yoimada <rudenenet@yahoo.co.jp>
2016-12-22 01:51:35 +0900

    Update getOrig.js

--- getOrig.js
+++ getOrig.js
@@ -1,1 +1,9 @@
-(function (){t=window.location.href;t=t.replace(":large",":orig");if ( t.indexOf(":orig") == -1 ) t=t+":orig";window.location.href=t;})();//javascript:(function (){ t=window.location.href; t=t.replace(":large",":orig"); if ( t.indexOf(":orig") == -1 ) t=t+":orig"; window.location.href=t;})(
+(function ()
+ {
+  t=window.location.href;
+  t=t.replace(":large",":orig");
+  if ( t.indexOf(":orig") == -1 ) t=t+":orig";
+  window.location.href=t;
+})();
+
+//javascript:(function (){ t=window.location.href; t=t.replace(":large",":orig"); if ( t.indexOf(":orig") == -1 ) t=t+":orig"; window.location.href=t;})();
