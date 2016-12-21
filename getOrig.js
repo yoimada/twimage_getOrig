70661d7a9dba237eaa6b4502c1c74fed9ad19992
yoimada <rudenenet@yahoo.co.jp>
2016-12-22 01:49:49 +0900

    Create getOrig.js

---
+++ getOrig.js
@@ -0,0 +1,1 @@
+(function (){t=window.location.href;t=t.replace(":large",":orig");if ( t.indexOf(":orig") == -1 ) t=t+":orig";window.location.href=t;})();//javascript:(function (){ t=window.location.href; t=t.replace(":large",":orig"); if ( t.indexOf(":orig") == -1 ) t=t+":orig"; window.location.href=t;})(
